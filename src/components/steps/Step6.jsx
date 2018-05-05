d2a4a2295b0764ccb14b6adfa53ab69f13ff8de5
Dominik Hanke <mail@dominik-hanke.de>
2018-05-05 19:26:03 +0200

    Recreated task list

--- src/components/steps/Step6.jsx
+++ src/components/steps/Step6.jsx
@@ -5,7 +5,52 @@ class Step6 extends React.Component {
   constructor(props) {
     super(props);
     this.props.updateStep(6);
+
+    this.state = {
+      tasks: {
+        "6ff1ebfe-2b56-493c-83b8-c0e5ff45fcaa": {
+          uuid: "6ff1ebfe-2b56-493c-83b8-c0e5ff45fcaa",
+          text: "Get your first appointments at the closest physiotherapist.",
+          done: true
+        },
+        "922b1464-d8f3-4668-b3b2-37d59492f3a6": {
+          uuid: "922b1464-d8f3-4668-b3b2-37d59492f3a6",
+          text:
+            "Get an appointment at your primary doctor and bring your documents.",
+          done: false
+        },
+        "ea9b3cc6-8996-4c64-96f4-d6e1dc34649f": {
+          uuid: "ea9b3cc6-8996-4c64-96f4-d6e1dc34649f",
+          text: "Go to the nearest medical supply store to get a custom brace.",
+          done: false
+        },
+        "1f3a623d-a022-4cd7-a586-62e8bf51cf4e": {
+          uuid: "1f3a623d-a022-4cd7-a586-62e8bf51cf4e",
+          text: "Get a new pair of shoes without laces, if you don't have one.",
+          done: false
+        },
+        "1630c4e4-cbbf-49c0-a2c5-9960a5902136": {
+          uuid: "1630c4e4-cbbf-49c0-a2c5-9960a5902136",
+          text:
+            "Check your entitlement to sickness benefit with your health insurance.",
+          done: false
+        },
+        "2dd91b37-fc7c-4faa-ae6d-f7889410bfab": {
+          uuid: "2dd91b37-fc7c-4faa-ae6d-f7889410bfab",
+          text:
+            "Inform your employer that you might not be able to work for several months.",
+          done: false
+        }
+      }
+    };
   }
+
+  toggleDone = uuid => {
+    this.setState(state => {
+      state.tasks[uuid].done = !this.state.tasks[uuid].done;
+      return state;
+    });
+  };
 
   render() {
     return (
@@ -13,20 +58,17 @@ class Step6 extends React.Component {
         <h2>Your next tasks</h2>
 
         <ul className="list-group tasks">
-          <TaskItem
-            text="Get an appointment at a physiotherapist."
-            done={false}
-          />
-          <TaskItem
-            text="Get an appointment at your primary doctor."
-            done={false}
-          />
-          <TaskItem text="Go to the next medical supply store." done={false} />
-          <TaskItem text="Get shoes without laces." done={false} />
-          <TaskItem
-            text="Check your entitlement to sickness benefit."
-            done={false}
-          />
+          {Object.keys(this.state.tasks).map(uuid => {
+            const task = this.state.tasks[uuid];
+
+            return (
+              <TaskItem
+                task={task}
+                key={task.uuid}
+                toggleDone={this.toggleDone}
+              />
+            );
+          })}
         </ul>
 
         <br />
--- src/components/steps/TaskItem.jsx
+++ src/components/steps/TaskItem.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 
 class TaskItem extends React.Component {
+  toggleDone = uuid => {
+    this.props.toggleDone(uuid);
+  };
+
   render() {
-    return <div className="task-item">Task Text here</div>;
+    const task = this.props.task;
+    const id = `task-${task.uuid}`;
+
+    return (
+      <li className="list-group-item">
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value={task.uuid}
+            id={id}
+            checked={task.done}
+            onChange={e => {
+              this.toggleDone(task.uuid);
+            }}
+          />
+          <label
+            className={`form-check-label ${task.done ? "done" : ""}`}
+            htmlFor={id}
+          >
+            {task.text}
+          </label>
+        </div>
+      </li>
+    );
   }
 }
 
